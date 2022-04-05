de9cc914abb3596f1c6f74fc87365eafd1c3bed2
LorenzoCrespan <lorenzo.crespan@gmail.com>
2022-04-05 18:43:39 +0200

    Funzia

--- src/compiler/mainSimpLanPlus.java
+++ src/compiler/mainSimpLanPlus.java
@@ -6,20 +6,20 @@ import java.util.ArrayList;
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
 
-import parser.SimpLanLexer;
-import parser.SimpLanParser;
+import parser.SimpLanPlusLexer;
+import parser.SimpLanPlusParser;
 
 public class mainSimpLanPlus {
 	public static void main(String[] args) throws Exception {
 
-		String fileName = "prova.simplan";
+		String fileName = "/Users/cresp/Documents/Workspace/Unibo_Compilatori_Interpreti/src/prova.simplan";
 
 		FileInputStream is = new FileInputStream(fileName);
 		ANTLRInputStream input = new ANTLRInputStream(is);
-		SimpLanLexer lexer = new SimpLanLexer(input);
+		SimpLanPlusLexer lexer = new SimpLanPlusLexer(input);
 		CommonTokenStream tokens = new CommonTokenStream(lexer);
 
-		SimpLanParser parser = new SimpLanParser(tokens);
-		SimpLanVisitorImpl visitor = new SimpLanVisitorImpl();
+		SimpLanPlusParser parser = new SimpLanPlusParser(tokens);
+		//SimpLanPlusVisitorImpl visitor = new SimpLanPlusVisitorImpl();
 	}
 }
