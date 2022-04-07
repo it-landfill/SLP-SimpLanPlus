a897628325d49f1205fa743f5fcbba350870d261
Alessandro Benetton <aleben98@gmail.com>
2022-04-07 10:17:29 +0200

    Implemented int and bool type nodes

---
+++ src/ast/typeNode/intTypeNode.java
@@ -0,0 +1,33 @@
+package ast.typeNode;
+
+import ast.Node;
+import util.Environment;
+import util.SemanticError;
+
+import java.util.ArrayList;
+
+public class intTypeNode implements Node {
+
+	public intTypeNode() {
+	}
+
+	@Override
+	public String toPrint(String indent) {
+		return indent + "type: integer";
+	}
+
+	@Override
+	public Node typeCheck() {
+		return null;
+	}
+
+	@Override
+	public String codeGeneration() {
+		return null;
+	}
+
+	@Override
+	public ArrayList<SemanticError> checkSemantics(Environment env) {
+		return null;
+	}
+}
