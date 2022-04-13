4ee7f346fa697dc62e75942d0bdfc5822c2b25df
Alessandro Benetton <aleben98@gmail.com>
2022-04-13 18:53:13 +0200

    [PCEI-8-esercizio-2-semantic-check] Check semantic call node

--- src/ast/statementNode/CallNode.java
+++ src/ast/statementNode/CallNode.java
@@ -1,10 +1,12 @@
 package ast.statementNode;
 
 import ast.Node;
+import ast.STentry;
 import util.Environment;
 import util.SemanticError;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class CallNode implements Node {
 	private final String funcName;
@@ -48,6 +50,39 @@ public class CallNode implements Node {
 
 	@Override
 	public ArrayList<SemanticError> checkSemantics(Environment env) {
-		return null;
+		 ArrayList<SemanticError> errors = new ArrayList<>();
+		 HashMap<String, STentry> hm = env.getCurrentLevelSymTable();
+
+		 STentry f = null;
+
+		 int nl = env.nestingLevel;
+		 while (f == null && nl>=0) {
+			 hm = env.symTable.get(nl--);
+			 f = hm.getOrDefault(funcName,null);
+		 }
+
+		 if (f == null) {
+			 errors.add(new SemanticError("Fun " + funcName + " does not exist in scope"));
+		 } else {
+			 if (f.getnArgs() == -1) {
+				 errors.add(new SemanticError("Fun " + funcName + " is not a function"));
+			 }
+			 if (params != null) {
+				 if (f.getnArgs() != params.size()) {
+					 errors.add(new SemanticError("Parameter number for " + funcName + " does not match. Expected " + f.getnArgs() + ". Have" + params.size()));
+				 }
+			 } else {
+				 if (f.getnArgs() != 0) {
+					 errors.add(new SemanticError("Parameter number for " + funcName + " does not match. Expected " + f.getnArgs() + ". Have 0"));
+				 }
+			 }
+		 }
+
+		if (params != null) {
+			for (Node n : params) {
+				errors.addAll(n.checkSemantics(env));
+			}
+		}
+		return errors;
 	}
 }
