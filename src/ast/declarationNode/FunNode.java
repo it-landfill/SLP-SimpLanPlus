99b1488ad2f8bcbf2d59c7828896f7b348483336
Alessandro Benetton <aleben98@gmail.com>
2022-04-19 18:09:53 +0200

    [(no branch, rebasing PCEI-8-esercizio-2-semantic-check)] [PCEI-8-esercizio-2-semantic-check] Declaration semantic check

--- src/ast/declarationNode/FunNode.java
+++ src/ast/declarationNode/FunNode.java
@@ -63,9 +63,6 @@ public class FunNode implements Node {
 		if (returnType != null) entry = new STentry(env.nestingLevel, returnType, env.offset);
 		else entry = new STentry(env.nestingLevel, env.offset);
 
-		if (params != null) entry.setnArgs(params.size());
-		else entry.setnArgs(0);
-
 		//TODO: Check nested functions
 
 		// Se da errore la funzione esiste già
