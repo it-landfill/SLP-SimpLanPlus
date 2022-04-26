61826b0319474e114d7577ff3ba3a7affc6cfddf
LorenzoCrespan <lorenzo.crespan@gmail.com>
2022-04-26 19:29:36 +0200

    Add comments in English for mainSimpLanPlus

--- src/compiler/mainSimpLanPlus.java
+++ src/compiler/mainSimpLanPlus.java
@@ -16,29 +16,25 @@ import java.util.ArrayList;
 public class mainSimpLanPlus {
     public static void main(String[] args) throws Exception {
         String fileName = "src/TestSimpLanPlus/prova.slp";
-
         // File loading.
-        CharStream input = CharStreams.fromFileName(fileName);
-        // Generazione dell'oggetto Error Handler utile alla gestione degli errori lessicali.
+        CharStream inputFile = CharStreams.fromFileName(fileName);
+        // Generation of the error handler object useful for managing lexical errors.
         SimpLanPlusErrorParser errHandler = new SimpLanPlusErrorParser();
-
         // Lexical verification.
         System.out.println("[INFO] Starting lexical verification.");
-        SimpLanPlusLexer lexer = new SimpLanPlusLexer(input);
+        SimpLanPlusLexer lexer = new SimpLanPlusLexer(inputFile);
         lexer.addErrorListener(errHandler);
         CommonTokenStream tokens = new CommonTokenStream(lexer);
-
         SimpLanPlusParser parser = new SimpLanPlusParser(tokens);
         parser.addErrorListener(errHandler);
-
-        // Implentazione del visitor utile al scorre AST in DFS.
+        // Visitor implementation useful for scrolling abstract syntax tree in depth-first search.
         SimpLanPlusVisitorImpl visitor = new SimpLanPlusVisitorImpl();
-        // Il DFS prende come radice il primo token della grammatica (block)
-        // TODO: Controllare per la consegna finale che il nodo radice sia rimasto immutato.
+        // The depth-first search for the abstract syntax tree starts from the root node, in the
+        // case of the SimpLanPlus grammar.
+        // TODO: Check for final delivery that the root node has remained unchanged.
         Node ast = visitor.visit(parser.block());
-
-        // If errors have been identified in the lexical analysis phase, they are printed, a report file is
-        // generated, and the program stops.
+        // If errors have been identified in the lexical analysis phase, they are printed, a report
+        // file is generated, and the program stops.
         if (errHandler.hasMessages()) {
             System.out.println("[ERROR] There were some errors while parsing the program.");
             System.out.println(errHandler);
@@ -46,22 +42,23 @@ public class mainSimpLanPlus {
             System.out.println("[ERROR] Program parsing failed");
             return;
         }
-
-        System.out.println("[INFO] No lexical error found.\n[INFO] Starting semantic verification.");
-
+        System.out.println("[INFO] No lexical error found.");
+        // Semantic verification.
+        System.out.println("[INFO] Starting semantic verification.");
         Environment env = new Environment();
-        // Generazione di un array list che conterrà tutti gli errori semantici.
+        // Generation of an array list that will contain all semantic errors.
         ArrayList<SemanticError> err = ast.checkSemantics(env);
-
-        // If errors have been identified in the semantic analysis phase, they are printed and the program stops.
+        // If errors have been identified in the semantic analysis phase, they are printed, a report
+        // file is generated, and the program stops.
         if (err != null && err.size() > 0) {
             System.out.println("You had " + err.size() + " semantic errors:");
             for (SemanticError e : err)
                 System.out.println("\t" + e);
             System.out.println("[ERROR] Program parsing failed");
-        } else {
-            System.out.println("[INFO] No semantic error found.\n[SUCCESS] Program parsing completed");
+            return;
         }
+        System.out.println("[INFO] No semantic error found.");
 
+        System.out.println("[SUCCESS] Program parsing completed");
     }
 }
