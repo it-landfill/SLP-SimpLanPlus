22d8511f52e386cd0446fc719a2a20ac3412eab5
LorenzoCrespan <lorenzo.crespan@gmail.com>
2022-04-27 11:26:11 +0200

    Add comments in English for declarationNode files

--- src/ast/declarationNode/FunNode.java
+++ src/ast/declarationNode/FunNode.java
@@ -8,97 +8,108 @@ import util.SemanticError;
 
 import java.util.ArrayList;
 
+/**
+ * The FunNode class manages the "declaration: decFun;".
+ * Allowing to save and manage the characteristics that define a function such as
+ * return type, function identifier, input arguments, and the function body.
+ */
 public class FunNode implements Node {
-	private final Node returnType;
-	private final String funcName;
-	private final ArrayList<ArgNode> params;
-	private final Node block;
 
-	public FunNode(Node returnType, String funName, ArrayList<ArgNode> params, Node block) {
-		this.returnType = returnType;
-		this.funcName = funName;
-		this.params = params;
-		this.block = block;
-	}
+    /**
+     * Return type identifier of the function.
+     */
+    private final Node returnType;
+    /**
+     * Function name identifier.
+     */
+    private final String funcName;
+    /**
+     * Arraylist of ArgNode that the function needs in input.
+     */
+    private final ArrayList<ArgNode> params;
+    /**
+     * Reference to the node that identifies the body of the function.
+     */
+    private final Node block;
 
-	@Override
-	public String toPrint(String indent) {
-		StringBuilder out = new StringBuilder();
+    public FunNode(Node returnType, String funName, ArrayList<ArgNode> params, Node block) {
+        this.returnType = returnType;
+        this.funcName = funName;
+        this.params = params;
+        this.block = block;
+    }
 
-		out.append(indent).append("function");
-		if (returnType != null) out.append(returnType.toPrint(indent));
-		else out.append(" void ");
+    @Override
+    public String toPrint(String indent) {
+        StringBuilder out = new StringBuilder();
 
-		out.append(funcName);
+        out.append(indent).append("function");
+        if (returnType != null) out.append(returnType.toPrint(indent));
+        else out.append(" void ");
 
-		if (params != null && !params.isEmpty()) {
-			out.append(" params ");
-			for (Node n : params) {
-				out.append(n.toPrint(indent));
-			}
-		}
+        out.append(funcName);
 
-		out.append("block ").append(block.toPrint(indent));
+        if (params != null && !params.isEmpty()) {
+            out.append(" params ");
+            for (Node n : params) {
+                out.append(n.toPrint(indent));
+            }
+        }
 
-		return out.toString();
-	}
+        out.append("block ").append(block.toPrint(indent));
 
-	@Override
-	public Node typeCheck() {
-		return null;
-	}
+        return out.toString();
+    }
 
-	@Override
-	public String codeGeneration() {
-		return null;
-	}
+    @Override
+    public Node typeCheck() {
+        return null;
+    }
 
-	@Override
-	public ArrayList<SemanticError> checkSemantics(Environment env) {
-		ArrayList<SemanticError> errors = new ArrayList<>();
+    @Override
+    public String codeGeneration() {
+        return null;
+    }
 
-		//Gestisco il caso delle funzioni nested
-		if (env.baseFun != null) { //TODO: Con la nuova SymbolTable si può fare diversamente??
-			errors.add(new SemanticError("Functions can not be nested.\tYou are declaring function " + funcName + " inside the body of function " + env.baseFun + ", this is not allowed."));
-		}
-
-		STentry entry;
-
-		// Genero la entry per la symbol table
-		entry = new STentry(env.nestingLevel, returnType, env.offset, funcName);
-
-		// Se la funzione ha parametri formali, salvo il numero di questi
-		if (params != null) entry.setnArgs(params.size());
-		else entry.setnArgs(0);
-
-		// Se da errore la funzione esiste già
-		if (env.symbolTable.addToSymbolTable(entry)) {
-			errors.add(new SemanticError("Fun " + funcName + " already declared"));
-		}
-
-		env.nestingLevel++;
-		env.baseFun = funcName;
-
-		// Controllo gli argomenti
-		if (params != null) {
-			for (ArgNode a : params) {
-				errors.addAll(a.checkSemantics(env));
-				STentry tmp = new STentry(env.nestingLevel, a.getType(), env.offset, a.getArgName());
-				if (env.symbolTable.addToSymbolTable(tmp)) {
-					errors.add(new SemanticError("arg " + a.getArgName() + " used multiple times"));
-				}
-			}
-		}
-
-		// Controllo il corpo
-		if (block != null) {
-			errors.addAll(block.checkSemantics(env));
-		}
-
-		env.symbolTable.removeLevelFromSymbolTable(env.nestingLevel);
-		env.baseFun = null;
-		env.nestingLevel--;
-
-		return errors;
-	}
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        ArrayList<SemanticError> errors = new ArrayList<>();
+        // Management of nested functions, control of the baseFun field. If the variable is
+        // null, then the function you define is not inside the body of other functions. Otherwise,
+        // an error is reported.
+        // TODO: Con la nuova SymbolTable si può fare diversamente??
+        if (env.baseFun != null) {
+            errors.add(new SemanticError("Functions can not be nested.\tYou are declaring function " + funcName + " inside the body of function " + env.baseFun + ", this is not allowed."));
+        }
+        // Generation of the entry for the symbol table.
+        STentry entry = new STentry(env.nestingLevel, returnType, env.offset, funcName);
+        // If the function has formal parameters, save the number of them.
+        if (params != null) entry.setnArgs(params.size());
+        else entry.setnArgs(0);
+        // Attempt to add the entry to the symbol table. In case of failure, an error is reported.
+        if (env.symbolTable.addToSymbolTable(entry))
+            errors.add(new SemanticError("Fun " + funcName + " already declared"));
+        // Increased nesting level.
+        env.nestingLevel++;
+        env.baseFun = funcName;
+        // Check arguments if there is at least one.
+        if (params != null) {
+            for (ArgNode a : params) {
+                errors.addAll(a.checkSemantics(env));
+                STentry tmp = new STentry(env.nestingLevel, a.getType(), env.offset, a.getArgName());
+                if (env.symbolTable.addToSymbolTable(tmp))
+                    errors.add(new SemanticError("arg " + a.getArgName() + " used multiple times"));
+            }
+        }
+        // Check the body of the function.
+        // NOTE: Increase of the nesting level is done in the semantic check of the block.
+        if (block != null) errors.addAll(block.checkSemantics(env));
+        // If the function has parameters, they are removed from the symbol table.
+        // TODO: Verificare che non dia problemi il non eliminare in caso di nessun parametro per la funzione.
+        if (params != null) env.symbolTable.removeLevelFromSymbolTable(env.nestingLevel);
+        // Decreased nesting level.
+        env.baseFun = null;
+        env.nestingLevel--;
+        return errors;
+    }
 }
--- src/ast/declarationNode/VarNode.java
+++ src/ast/declarationNode/VarNode.java
@@ -7,49 +7,63 @@ import util.SemanticError;
 
 import java.util.ArrayList;
 
+/**
+ * The VarNode class manages the "declaration: decVar;".
+ * Allowing to save and manage the characteristics that define a variable declaration and assignment
+ * such as type, variable identifier, and the value.
+ */
 public class VarNode implements Node {
-	private final String ID;
-	private final Node type;
-	private final Node exp;
+    /**
+     * Variable name identifier.
+     */
+    private final String ID;
+    /**
+     * Variable type identifier.
+     */
+    private final Node type;
+    /**
+     * Identifier of the expression to be assigned to the variable
+     */
+    private final Node exp;
 
-	public VarNode(String ID, Node type, Node exp) {
-		this.ID = ID;
-		this.type = type;
-		this.exp = exp;
-	}
+    public VarNode(String ID, Node type, Node exp) {
+        this.ID = ID;
+        this.type = type;
+        this.exp = exp;
+    }
 
-	public VarNode(String ID, Node type) {
-		this(ID, type, null);
-	}
+    public VarNode(String ID, Node type) {
+        this(ID, type, null);
+    }
 
-	@Override
-	public String toPrint(String indent) {
-		StringBuilder out = new StringBuilder();
-		out.append(indent).append("var: ").append(type.toPrint(indent)).append(" ").append(ID);
-		if (exp != null) out.append(" ").append(exp.toPrint(indent));
-		return out.toString();
-	}
+    @Override
+    public String toPrint(String indent) {
+        StringBuilder out = new StringBuilder();
+        out.append(indent).append("var: ").append(type.toPrint(indent)).append(" ").append(ID);
+        if (exp != null) out.append(" ").append(exp.toPrint(indent));
+        return out.toString();
+    }
 
-	@Override
-	public Node typeCheck() {
-		return null;
-	}
+    @Override
+    public Node typeCheck() {
+        return null;
+    }
 
-	@Override
-	public String codeGeneration() {
-		return null;
-	}
+    @Override
+    public String codeGeneration() {
+        return null;
+    }
 
-	@Override
-	public ArrayList<SemanticError> checkSemantics(Environment env) {
-		ArrayList<SemanticError> errors = new ArrayList<>();
-
-		STentry entry = new STentry(env.nestingLevel, type, env.offset, ID);
-
-		if (env.symbolTable.addToSymbolTable(entry)) errors.add(new SemanticError("Var " + ID + " already declared"));
-
-		if (exp != null) errors.addAll(exp.checkSemantics(env));
-
-		return errors;
-	}
+    @Override
+    public ArrayList<SemanticError> checkSemantics(Environment env) {
+        ArrayList<SemanticError> errors = new ArrayList<>();
+        // Generation of the entry for the symbol table.
+        STentry entry = new STentry(env.nestingLevel, type, env.offset, ID);
+        // Attempt to add the entry to the symbol table. In case of failure, an error is reported.
+        if (env.symbolTable.addToSymbolTable(entry))
+            errors.add(new SemanticError("Var " + ID + " already declared"));
+        // Check the espressione.
+        if (exp != null) errors.addAll(exp.checkSemantics(env));
+        return errors;
+    }
 }
