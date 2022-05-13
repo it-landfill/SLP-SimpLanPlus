95f44347661191b5983dc579773baa7babda4a8c
Alessandro Benetton <aleben98@gmail.com>
2022-05-13 18:40:32 +0200

    [PCEI-12-code-generation] Generate SVMParser

---
+++ src/SVMParser/SVM.g4
@@ -0,0 +1,45 @@
+grammar SVM;
+
+@header {
+import java.util.HashMap;
+}
+
+@lexer::members {
+public int lexicalErrors=0;
+}
+
+/*------------------------------------------------------------------
+ * PARSER RULES
+ *------------------------------------------------------------------*/
+
+assembly: (instruction)* ;
+
+instruction:
+    ( PUSH n=NUMBER
+	  | POP
+	  | ADD
+	  | SUB
+	  | MULT
+	  | DIV
+	  | PRINT
+	  ) ;
+
+/*------------------------------------------------------------------
+ * LEXER RULES
+ *------------------------------------------------------------------*/
+
+PUSH  	 : 'push' ; 	// pushes constant in the stack
+POP	 : 'pop' ; 	// pops from stack
+ADD	 : 'add' ;  	// add two values from the stack
+SUB	 : 'sub' ;	// add two values from the stack
+MULT	 : 'mult' ;  	// add two values from the stack
+DIV	 : 'div' ;	// add two values from the stack
+PRINT	 : 'print' ;	// print top of stack
+
+COL	 : ':' ;
+NUMBER	 : '0' | ('-')?(('1'..'9')('0'..'9')*) ;
+
+WHITESP  : ( '\t' | ' ' | '\r' | '\n' )+   -> channel(HIDDEN);
+
+ERR   	 : . { System.err.println("Invalid char: "+ getText()); lexicalErrors++;  } -> channel(HIDDEN);
+
---
+++ src/SVMParser/SVMBaseVisitor.java
@@ -0,0 +1,31 @@
+// Generated from /Users/alessandrobenetton/Library/CloudStorage/OneDrive-AlmaMaterStudiorumUniversitàdiBologna/University/I_Anno/II_[81941]-Compilatori_e_Interpreti/Progetto_CEI/src/SVMParser/SVM.g4 by ANTLR 4.9.2
+package SVMParser;
+
+import java.util.HashMap;
+
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link SVMVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public class SVMBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements SVMVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAssembly(SVMParser.AssemblyContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitInstruction(SVMParser.InstructionContext ctx) { return visitChildren(ctx); }
+}
