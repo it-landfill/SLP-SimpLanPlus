1c0946ffe1ec04f0d84e554931c7ecbcd505d249
Alessandro Benetton <aleben98@gmail.com>
2022-08-24 16:57:03 +0200

    [PCEI-57-not-a-valid-instruction-0-at-position-9992] Fix nested env return

--- src/util/SLPUtils.java
+++ src/util/SLPUtils.java
@@ -3,6 +3,8 @@ package util;
 import SLP_ast.typeNode.*;
 
 import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SLPUtils {
 
@@ -44,6 +46,26 @@ public class SLPUtils {
 	public static class TypeCheckError extends Exception {
 		public TypeCheckError(String errorMessage) {
 			super(errorMessage);
+		}
+	}
+
+	public static void SBReplaceAll(StringBuilder sb, String find, String replace){
+
+		//compile pattern from find string
+		Pattern p = Pattern.compile(find);
+
+		//create new Matcher from StringBuilder object
+		Matcher matcher = p.matcher(sb);
+
+		//index of StringBuilder from where search should begin
+		int startIndex = 0;
+
+		while( matcher.find(startIndex) ){
+
+			sb.replace(matcher.start(), matcher.end(), replace);
+
+			//set next start index as start of the last match + length of replacement
+			startIndex = matcher.start() + replace.length();
 		}
 	}
 }
